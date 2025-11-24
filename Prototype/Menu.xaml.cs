1c8eb6b5567b1917111ce555c8fb704b1f146087
AdamG6754 <adam.goltsman@gmail.com>
2025-11-24 17:25:22 -0500

    Added a rudimentary menu

--- Prototype/Menu.xaml.cs
+++ Prototype/Menu.xaml.cs
@@ -18,4 +18,60 @@ public partial class Menu : ContentPage
         // Navigate to the Login page using its route name
         await Shell.Current.GoToAsync(nameof(Login));
     }
+
+    private async void OnFoodSelected(object sender, EventArgs e) 
+    {
+        string selected = Foods.SelectedItem.ToString();
+
+        switch (selected)
+        {
+            case "Mains":
+                one.Text = "Cheeseurger";
+                two.Text = "Tacos (3)";
+                three.Text = "Chicken tenders (3)";
+                four.Text = "Salad";
+
+                bone.IsVisible = true;
+                btwo.IsVisible = true;
+                bthree.IsVisible = true;
+                bfour.IsVisible = true;
+                break;
+
+            case "Sides":
+                one.Text = "Fries";
+                two.Text = "Mozzarella sticks";
+                three.Text = "Onion rings";
+                four.Text = "Chips";
+
+                bone.IsVisible = true;
+                btwo.IsVisible = true;
+                bthree.IsVisible = true;
+                bfour.IsVisible = true;
+                break;
+
+            case "Drinks":
+                one.Text = "Water bottle";
+                two.Text = "Cola";
+                three.Text = "Diet cola";
+                four.Text = "Mtn Dew: Baja Blast";
+
+                bone.IsVisible = true;
+                btwo.IsVisible = true;
+                bthree.IsVisible = true;
+                bfour.IsVisible = true;
+                break;
+
+            case "Desserts":
+                one.Text = "Churro";
+                two.Text = "Cookie";
+                three.Text = "Brownie";
+                four.Text = "Donut";
+
+                bone.IsVisible = true;
+                btwo.IsVisible = true;
+                bthree.IsVisible = true;
+                bfour.IsVisible = true;
+                break;
+        }
+    }
 }
