1a8217d6123ba45e00bd18db45ee49e384106ed7
Sébastien Guillotin <sebgusa@gmail.com>
2025-12-02 22:53:25 -0500

    Progress on LoginManager.cs
    Cleaned comments, added and tailored code, began login and register methods. Finished class constructor method incl File IO and read/write in mehods.
    
    Gotta connect to frontend bc I realized this is prob not looking for a txt file bc it's never called...
    
    Let's go fix that.
    
    ^-^

--- Prototype/LoginManager.cs
+++ Prototype/LoginManager.cs
@@ -1,38 +1,59 @@
 namespace MauiApp1;
 using System.IO;
+using System.Text.Json;
 
 public class LoginManager
 {
     // create a blueprint for our login managers
-    // TODO code here
-
-    // Dictionary<user,password> login(?)= new Dictionary<user,password>();
-    // Dictionary<string,string> login= new Dictionary<string,string>();
     private Dictionary<string, string> login;
+    // if login needs to be read only uncomment below line
+    // private readonly Dictionary<string, string> login;
+    // we need write abilites though, rite?
 
     public LoginManager()
     {
         // file io stuff here
+        // 1. load from file to directory
+
+        //gets file JSON data, converts back to dictionary
+        StreamReader userRead = new StreamReader("Resources/accounts_data.txt");
+        string fileData = userRead.ReadToEnd();
+
         // deserialize from file to dictionary
-        this.login = new Dictionary<string, string>();
-        // add stuff from userRef here
+        login = JsonSerializer.Deserialize<Dictionary<string, string>>(fileData);
+        userRead.Close();
     }
 
-    // login(?).Add(param1, param2);
-
-
-    // 1. load from file to directory
 
     // 2. validate login
     // pwd matches stored?
     // return true or false accordingly
+    public bool ValidateLogin(string username, string password)
+    {
+        // check if username exists in dictionary
+        if (login.ContainsKey(username))
+        {
+            // check if password matches
+            return login[username] == password;
+        }
+        return false;
+    }
 
     // 3. register new user (write to file)
+    // adds key, value to dictionary, parses into JSON, writes to file
+    public static void RegisterNewUser(string username, string password, Dictionary<string, string> database)
+    {
+        // add new key, value pair to dictionary
+        database[username] = password;
+        // Append, or read and re-write with new info, to text file
+        string jsonData = JsonSerializer.Serialize(database);
+        StreamWriter userWrite = new StreamWriter("Resources/accounts_data.txt", false);
+        userWrite.Write(jsonData);
+        // close stream
+        userWrite.Close();
+    }
 
-    // If user already exists return false
-    // If not append, or read and re-write with new info, to text file
-    // format username : password (whatever dictionary needs to have key:value pairs)
-
-    // TODO code here
-
+        // If user already exists return false
+        // Append, or read and re-write with new info, to text file
+        // format username : password (whatever dictionary needs to have key:value pairs)
 }
