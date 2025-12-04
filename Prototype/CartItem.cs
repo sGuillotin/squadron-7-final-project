28dde5d94faaefc3f22a30f7fbd277fe29b4c492
Brandon K <brandk0313@gmail.com>
2025-12-04 08:56:22 -0500

    Create CartItem.cs

---
+++ Prototype/CartItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1;
+
+public class CartItem
+{
+    public string Name { get; set; }
+    public int Quantity { get; set; }
+}
