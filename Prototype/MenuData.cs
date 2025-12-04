878243130b5057357c0f5c018bd25663bdc64a7a
TheSeventhStone <sgl1009@rit.edu>
2025-12-04 09:05:39 -0500

    Added accessors to MenuData

--- Prototype/MenuData.cs
+++ Prototype/MenuData.cs
@@ -16,5 +16,23 @@ namespace MauiApp1
             this.menuPages = new Dictionary<string, Dictionary<string, Food>>();
             this.menuPages["page"]["food"] = new Food("food", 1.00);
         }
+        //adds page to menu
+        public void addPage(string page)
+        {
+            this.menuPages[page] = new Dictionary<string, Food>();
+        }
+        //adds Food item to menu page
+        public void addFood(string page, string food, double price)
+        {
+            this.menuPages[page][food] = new Food(food, price);
+        }
+        //gets menu page
+        public Dictionary<string,Food> getPage(string page) {
+            return this.menuPages[page];
+        }
+        //gets Food item
+        public Food getFood(string page, string food) {
+            return this.menuPages[page][food];
+        }
     }
 }
